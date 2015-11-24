7d2c601cb9f63ee04ac025b5dcb35c5ca9a410f9
Chase Latta <chaselatta@gmail.com>
2015-11-23 21:37:47 -0800

    Add sample project outline

---
+++ Channels iOS Example/Pods/Headers/Private/Channels/Channels.h
@@ -0,0 +1,1 @@
+../../../../../Channels/Channels.h
---
+++ Channels iOS Example/Pods/Target Support Files/Channels/Channels-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "Channels.h"
+
+FOUNDATION_EXPORT double ChannelsVersionNumber;
+FOUNDATION_EXPORT const unsigned char ChannelsVersionString[];
+
